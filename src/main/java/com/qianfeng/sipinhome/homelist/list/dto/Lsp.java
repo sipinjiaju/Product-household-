7546dc2c9db834cf1920bf7b91ccc445b3563081
WH-Zss <863649577@qq.com>
2019-05-15 19:47:19 +0800

    初始提交3

---
+++ src/main/java/com/qianfeng/sipinhome/homelist/list/dto/Lsp.java
@@ -0,0 +1,13 @@
+package com.qianfeng.sipinhome.homelist.list.dto;
+
+public class Lsp {
+    private int L;
+
+    public int getL() {
+        return L;
+    }
+
+    public void setL(int l) {
+        L = l;
+    }
+}
