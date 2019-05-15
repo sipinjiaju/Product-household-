01070d92832b9407d7560d729d72a557f6a5b36c
GZlsp0810 <527854950@qq.com>
2019-05-15 18:05:56 +0800

    项目环境的搭建

---
+++ src/main/java/com/qianfeng/sipinhome/SipinhomeApplication.java
@@ -0,0 +1,13 @@
+package com.qianfeng.sipinhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipinhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipinhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/com.qianfeng/sipinhome/SipinhomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.com.qianfeng.sipinhome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SipinhomeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
