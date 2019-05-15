7f2f588bf2c7ecdb7f0a0389eb6171814ba727c0
GZlsp0810 <527854950@qq.com>
2019-05-15 19:32:48 +0800

    斯品家居项目初步搭建

---
+++ src/main/java/com/qianfeng/sipinhome/homelist/list/dto/home.java
@@ -0,0 +1,13 @@
+package com.qianfeng.sipinhome.homelist.list.dto;
+
+public class home {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
