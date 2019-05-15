2649b2baa1a2746321df6538e507982d4c18798a
WH-Zss <863649577@qq.com>
2019-05-15 19:36:55 +0800

    初始提交2

---
+++ src/main/java/com/qianfeng/sipinhome/user/User.java
@@ -0,0 +1,13 @@
+package com.qianfeng.sipinhome.user;
+
+public class User {
+    private String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
