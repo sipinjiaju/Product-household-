a2e6aa65813c703f11d9d68253cb7672d3f4621e
GZlsp0810 <527854950@qq.com>
2019-05-16 14:15:09 +0800

    初始提交李

--- src/main/java/com/qianfeng/sipinhome/bedroom/list/dto/bedroom.java
+++ src/main/java/com/qianfeng/sipinhome/bedroom/list/dto/bedroom.java
@@ -3,6 +3,15 @@ package com.qianfeng.sipinhome.bedroom.list.dto;
 public class bedroom {
     private int id;
     private String name;
+    private String fie;
+
+    public String getFie() {
+        return fie;
+    }
+
+    public void setFie(String fie) {
+        this.fie = fie;
+    }
 
     public int getId() {
         return id;
